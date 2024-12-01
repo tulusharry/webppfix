3626f77039c1a918a9b9485690b8951c6df15873
tulusharry <87703966+tulusharry@users.noreply.github.com>
2024-12-01 16:52:02 +0700

    Update googlesheet.js

--- js/googlesheet.js
+++ js/googlesheet.js
@@ -1,11 +1,13 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzbectgcL_laQs0vTC342JfTK_6g1my8iY3eTcoig2S3wvdlSdZD45kOm5UYkbRcrVD/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzonviMNfafouJC_tQMCox_RSRDjkrJ0h4cRda1M4sd7PR-x5sLw1XMmMgTA4rCWk-n/exec'
 
 const form = document.forms['contact-form']
 
 form.addEventListener('submit', e => {
-    e.prevenDefault()
-    fetch(scriptURL, { method: 'Post', body: new FormData(form) })
-        .then(response => alert("Thank you! your form is submitted succesfully."))
+
+    e.preventDefault()
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => alert("Thank you! Form is submitted"))
         .then(() => { window.location.reload(); })
         .catch(error => console.error('Error!', error.message))
-})  
+})
